6d290aeff3d1d32978d9277912744a4e1b242d9b
Alicja Begović <alicja.begovic@gmail.com>
2024-03-22 17:19:23 +0100

    selecting two cards with result and logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import helmet from "./img/helmet-1.png";
 import potion from "./img/potion-1.png";
 import ring from "./img/ring-1.png";
@@ -20,6 +20,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   //shuffle cards
   const shuffleCards = () => {
@@ -31,7 +33,30 @@ function App() {
     setTurns(0);
   };
 
-  console.log(cards, turns);
+  //handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  //compare 2 selected cards
+  useEffect(() => {
+    if(choiceOne && choiceTwo) {
+      if(choiceOne.src === choiceTwo.src) {
+        console.log("Correct");
+        resetTurns();
+      } else {
+        console.log("Incorrect")
+        resetTurns();
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  //reset choices & increase turn
+  const resetTurns = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(prevTurns => prevTurns + 1);
+  }
 
   return (
     <div className="App">
@@ -39,7 +64,7 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
@@ -51,5 +76,8 @@ export default App;
 /*
 
 const shuffleCards = [...cardImages, ...cardImages]; - writing two times making two the same arrays (because we need 1 pair of each picture)
+
+const [choiceOne, setChoiceOne] = useState(null);
+const [choiceTwo, setChoiceTwo] = useState(null); - we are choosing 2 cards so needs to be 2 states
 
 */
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,21 @@
 import cover from "../img/cover.png";
 import "./SingleCard.css";
 
-export default function SingleCard({ card }) {
+export default function SingleCard({ card, handleChoice }) {
+
+    const handleClick = () => {
+        handleChoice(card);
+    }
   return (
     <div className="card" key={card.id}>
       <div>
         <img className="front" src={card.src} alt="card front" />
-        <img className="back" src={cover} alt="card back" />
+        <img
+          className="back"
+          src={cover}
+          onClick={handleClick}
+          alt="card back"
+        />
       </div>
     </div>
   );
